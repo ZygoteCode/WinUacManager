9c22e2d781a5477a7fc0a03462631c426769166b
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-08-03 19:37:42 +0200

    First commit

---
+++ Utils/WindowsUtils.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+
+public class WindowsUtils
+{
+    public static string GetWindowsFriendlyName()
+    {
+        string ProductName = GetHKEYLocalMachineString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ProductName");
+        string CSDVersion = GetHKEYLocalMachineString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "CSDVersion");
+
+        if (ProductName != "")
+        {
+            return (ProductName.StartsWith("Microsoft") ? "" : "Microsoft ") +
+                ProductName + (CSDVersion != "" ? " " + CSDVersion : "");
+        }
+
+        return "";
+    }
+
+    private static string GetHKEYLocalMachineString(string path, string key)
+    {
+        try
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(path);
+
+            if (registryKey == null)
+            {
+                return "";
+            }
+
+            return (string)registryKey.GetValue(key);
+        }
+        catch
+        {
+            return "";
+        }
+    }
+
+    public static void SetDWord(string key, string name, int value)
+    {
+        try
+        {
+            Registry.LocalMachine.CreateSubKey(key).SetValue(name, value.ToString(), RegistryValueKind.DWord);
+        }
+        catch
+        {
+
+        }
+
+        try
+        {
+            Registry.CurrentUser.CreateSubKey(key).SetValue(name, value.ToString(), RegistryValueKind.DWord);
+        }
+        catch
+        {
+
+        }
+    }
+}
