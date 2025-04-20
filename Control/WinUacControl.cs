9bae36bddeff7ccc64b8d619a786fa95278f3edf
ZygoteCode <zygotecode@proton.me>
2025-04-20 22:16:04 +0200

    First commit

---
+++ Control/WinUacControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class WinUacControl
+{
+    private const string UacRegistryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System";
+
+    public static void EnableUserAccountControl()
+    {
+        SetUacRegistryKey("EnableLUA", 1);
+        SetUacRegistryKey("PromptOnSecureDesktop", 1);
+        SetUacRegistryKey("ConsentPromptBehaviorAdmin", 5);
+        SetUacRegistryKey("ConsentPromptBehaviorUser", 3);
+        SetUacRegistryKey("EnableVirtualization", 1);
+        SetUacRegistryKey("EnableInstallerDetection", 1);
+        SetUacRegistryKey("FilterAdministratorToken", 0);
+        SetUacRegistryKey("EnableAdminApprovalMode", 1);
+        SetUacRegistryKey("ValidateAdminCodeSignatures", 1);
+        SetUacRegistryKey("EnableSecureUIAPaths", 1);
+        SetUacRegistryKey("EnableUIADesktopToggle", 0);
+        SetUacRegistryKey("LocalAccountTokenFilterPolicy", 0);
+        SetUacRegistryKey("EnableSwitchToRedesignedSafeDesktop", 1);
+        SetUacRegistryKey("InteractiveLogonFirst", 0);
+    }
+
+    public static void DisableUserAccountControl()
+    {
+        SetUacRegistryKey("EnableLUA", 0);
+        SetUacRegistryKey("PromptOnSecureDesktop", 0);
+        SetUacRegistryKey("ConsentPromptBehaviorAdmin", 0);
+        SetUacRegistryKey("ConsentPromptBehaviorUser", 0);
+        SetUacRegistryKey("EnableVirtualization", 0);
+        SetUacRegistryKey("EnableInstallerDetection", 0);
+        SetUacRegistryKey("FilterAdministratorToken", 1);
+        SetUacRegistryKey("EnableAdminApprovalMode", 0);
+        SetUacRegistryKey("ValidateAdminCodeSignatures", 0);
+        SetUacRegistryKey("EnableSecureUIAPaths", 0);
+        SetUacRegistryKey("EnableUIADesktopToggle", 0);
+        SetUacRegistryKey("LocalAccountTokenFilterPolicy", 1);
+        SetUacRegistryKey("EnableSwitchToRedesignedSafeDesktop", 0);
+        SetUacRegistryKey("InteractiveLogonFirst", 0);
+    }
+
+    private static void SetUacRegistryKey(string keyName, int value)
+    {
+        try
+        {
+            WindowsUtils.SetDWord(UacRegistryPath, keyName, value);
+        }
+        catch
+        {
+
+        }
+    }
+}
---
+++ Utils/WindowsUtils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.Win32;
+
+public class WindowsUtils
+{
+    public static string GetWindowsFriendlyName()
+    {
+        string ProductName = GetHKEYLocalMachineString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ProductName");
+        string CSDVersion = GetHKEYLocalMachineString(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion", "CSDVersion");
+
+        if (ProductName != "")
+        {
+            return (ProductName.StartsWith("Microsoft") ? "" : "Microsoft ") +
+                ProductName + (CSDVersion != "" ? " " + CSDVersion : "");
+        }
+
+        return "";
+    }
+
+    private static string GetHKEYLocalMachineString(string path, string key)
+    {
+        try
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(path);
+
+            if (registryKey == null)
+            {
+                return "";
+            }
+
+            return (string)registryKey.GetValue(key);
+        }
+        catch
+        {
+            return "";
+        }
+    }
+
+    public static void SetDWord(string keyPath, string valueName, int value)
+    {
+        try
+        {
+            using (RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(keyPath, true))
+            {
+                if (registryKey != null)
+                {
+                    registryKey.SetValue(valueName, value, RegistryValueKind.DWord);
+                }
+            }
+        }
+        catch
+        {
+
+        }
+
+        try
+        {
+            using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(keyPath, true))
+            {
+                if (registryKey != null)
+                {
+                    registryKey.SetValue(valueName, value, RegistryValueKind.DWord);
+                }
+            }
+        }
+        catch
+        {
+
+        }
+    }
+}
